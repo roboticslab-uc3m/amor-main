570c62533c50369aca3d5ac7ae021b20ef312b2f
Bartek Łukawski <bwmn.peter@gmail.com>
2018-03-16 15:53:02 +0100

    Orchestrate superbuilds

---
+++ cmake/ycm-modules/BuildAMOR_API.cmake
@@ -0,0 +1,10 @@
+include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(YARP QUIET) # cartesianRate program
+
+ycm_ep_helper(AMOR_API TYPE GIT
+              STYLE GITHUB
+              REPOSITORY roboticslab-uc3m/amor-api.git
+              TAG master
+              DEPENDS YARP)
