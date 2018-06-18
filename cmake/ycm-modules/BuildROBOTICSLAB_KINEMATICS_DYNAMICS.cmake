5cd22b04056e1828e0dd33443f3d71685c0373b3
Bartek Łukawski <bwmn.peter@gmail.com>
2018-06-18 18:28:36 +0200

    Pull color-debug via YCM

--- cmake/ycm-modules/BuildROBOTICSLAB_KINEMATICS_DYNAMICS.cmake
+++ cmake/ycm-modules/BuildROBOTICSLAB_KINEMATICS_DYNAMICS.cmake
@@ -3,6 +3,7 @@ include(FindOrBuildPackage)
 
 find_or_build_package(orocos_kdl QUIET)
 find_or_build_package(YARP QUIET)
+find_or_build_package(COLOR_DEBUG QUIET)
 find_or_build_package(AMOR_API QUIET)
 find_or_build_package(ROBOTICSLAB_YARP_DEVICES QUIET) # proximity sensors
 
@@ -10,4 +11,4 @@ ycm_ep_helper(ROBOTICSLAB_KINEMATICS_DYNAMICS TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/kinematics-dynamics.git
               TAG master
-              DEPENDS "orocos_kdl;YARP;AMOR_API;ROBOTICSLAB_YARP_DEVICES")
+              DEPENDS "orocos_kdl;YARP;COLOR_DEBUG;AMOR_API;ROBOTICSLAB_YARP_DEVICES")
