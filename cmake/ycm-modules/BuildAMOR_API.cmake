4440fb8140e92d8801af7f59435494b3fb9af8d8
Bartek Łukawski <bwmn.peter@gmail.com>
2021-05-08 13:48:07 +0200

    Review CMake code and interdependencies

--- cmake/ycm-modules/BuildAMOR_API.cmake
+++ cmake/ycm-modules/BuildAMOR_API.cmake
@@ -1,11 +1,8 @@
 include(YCMEPHelper)
-include(FindOrBuildPackage)
 
-find_or_build_package(YARP QUIET) # cartesianRate program
 
 ycm_ep_helper(AMOR_API TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/amor-api.git
               TAG master
-              DEPENDS YARP
               CMAKE_CACHE_ARGS "-DENABLE_udev_rules:BOOL=OFF")
--- cmake/ycm-modules/BuildROBOTICSLAB_YARP_DEVICES.cmake
+++ cmake/ycm-modules/BuildROBOTICSLAB_YARP_DEVICES.cmake
@@ -3,10 +3,9 @@ include(FindOrBuildPackage)
 
 find_or_build_package(YARP QUIET)
 find_or_build_package(AMOR_API QUIET)
-find_or_build_package(COLOR_DEBUG QUIET)
 
 ycm_ep_helper(ROBOTICSLAB_YARP_DEVICES TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/yarp-devices.git
               TAG master
-              DEPENDS "YARP;AMOR_API;COLOR_DEBUG")
+              DEPENDS "YARP;AMOR_API")
