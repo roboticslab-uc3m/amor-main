a9c5679fa6bff780ded3f640d19ef5379c7df6ba
Bartek Łukawski <PeterBowman@users.noreply.github.com>
2026-07-29 09:28:07 +0200

    Pull KDL from apt

--- cmake/ycm-modules/Buildorocos_kdl.cmake
+++
@@ -1,7 +0,0 @@
-include(YCMEPHelper)
-
-ycm_ep_helper(orocos_kdl TYPE GIT
-              STYLE GITHUB
-              REPOSITORY orocos/orocos_kinematics_dynamics.git
-              TAG master
-              SOURCE_SUBDIR orocos_kdl)
